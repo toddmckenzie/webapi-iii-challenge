82405db58307a0b3670761b79dcb5d8c2467001f
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-05-29 15:14:38 -0500

    setup for middleware

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
 
@@ -9,7 +9,29 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
+  //console.log( request method, request url, timestamp )
 
+};
+
+function validateUserId(req, res, next) {
+  //validates user id on every request taht expects a user id param.
+  //if id is valid, store the user onj as req.user
+  //if id param doesnt amtch an user id in database. cancel request and repsond status 400 { message: 'invalid user id' }
+
+};
+
+function validateUser(req, res, next) {
+  //validates the body on the request to create new user
+  //if body is missing,cancel request and repsond with status 400 and { message: 'missing user data' }
+  //if req body is missing required name field, cancel request and repsond status 400 and { message: 'missing required name field'}
+
+};
+
+function validatePost(req, res, next) {
+  //validates the body on a request to create a new post
+  //if request body msising, cancel request and repsond status 400 and { message: 'missing post data'}
+  //if request body missing the required text field, cancel request and respond status 400 and { message: 'missing required text field' }
+  
 };
 
 module.exports = server;
